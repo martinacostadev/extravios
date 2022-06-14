67f4f3664885cf06f93ee846c0f1cb2a3382c44e
martindevaluado <pmartinacosta@gmail.com>
2022-06-14 20:22:39 -0300

    refactor: create PostItem component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,43 +7,23 @@ import {
   Heading,
   Text,
   Flex,
-  Spacer,
   Input,
   Button,
   Center,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  useDisclosure,
-  ModalFooter,
-  IconButton,
   InputGroup,
   InputLeftElement,
 } from '@chakra-ui/react'
 import { Posts } from 'interfaces'
 
-import { BsWhatsapp } from 'react-icons/bs'
-import { FiShare2 } from 'react-icons/fi'
 import { BiSearch } from 'react-icons/bi'
 
 import { server } from 'config'
 
 import debounce from 'just-debounce-it'
-import { getTimeAgo } from 'utils/common'
 
 import Error from 'next/error'
 
-import {
-  WhatsappShareButton,
-  WhatsappIcon,
-  FacebookShareButton,
-  FacebookIcon,
-  TwitterShareButton,
-  TwitterIcon,
-} from 'next-share'
+import PostItem from 'components/PostItem'
 
 interface Props {
   response: {
@@ -85,25 +65,6 @@ export default function Home({ response, errorCode }: Props) {
   const SHOW_MORE_BUTTON = count > posts?.length
   const IS_SEARCHING = Boolean(searchInput.current?.value?.length)
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [modalInfo, setModalInfo] = React.useState({
-    title: '',
-    description: '',
-    whatsApp: '',
-    url: '',
-  })
-
-  const OverlayTwo = () => (
-    <ModalOverlay
-      bg="none"
-      backdropFilter="auto"
-      backdropInvert="80%"
-      backdropBlur="2px"
-    />
-  )
-
-  const [overlay, setOverlay] = React.useState(<OverlayTwo />)
-
   if (errorCode) {
     return <Error statusCode={errorCode} />
   }
@@ -130,69 +91,8 @@ export default function Home({ response, errorCode }: Props) {
     setPosts((prev) => [...prev, ...postsData])
   }
 
-  const handleWhatsApp = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    title: string
-  ) => {
-    event.preventDefault()
-    const MESSAGE = title.replace(' ', '%20')
-
-    const url = `https://wa.me/5211234567890?text=Hola!%20Vi%20tu%20pubilacion%20${MESSAGE}`
-    window.open(url, '_blank')
-  }
-
-  const handleShare = (
-    event: React.MouseEvent<HTMLButtonElement>,
-    post: Posts
-  ) => {
-    event.preventDefault()
-    const postData = {
-      title: post?.title,
-      description: post?.description,
-      whatsApp: post?.whatsApp,
-      url: `${server}/post/${post?.id}`,
-    }
-    setModalInfo(postData)
-    setOverlay(<OverlayTwo />)
-    onOpen()
-  }
-
   return (
     <Container py={4} centerContent>
-      <Modal size="sm" isCentered isOpen={isOpen} onClose={onClose}>
-        {overlay}
-        <ModalContent>
-          <ModalHeader>Compartir</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Flex minWidth="max-content" justifyContent="center" gap={4} p={4}>
-              <FacebookShareButton
-                url={modalInfo?.url}
-                quote={modalInfo?.description}
-                hashtag={'#nextshare'}
-              >
-                <FacebookIcon size={48} round />
-              </FacebookShareButton>
-
-              <WhatsappShareButton
-                url={modalInfo?.url}
-                title={modalInfo?.title}
-                separator=":: "
-              >
-                <WhatsappIcon size={48} round />
-              </WhatsappShareButton>
-
-              <TwitterShareButton url={modalInfo?.url} title={modalInfo?.title}>
-                <TwitterIcon size={48} round />
-              </TwitterShareButton>
-            </Flex>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>Cerrar</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
       <Box
         padding="4"
         bg="cyan-900"
@@ -219,73 +119,10 @@ export default function Home({ response, errorCode }: Props) {
         </Flex>
         <HomeSearch ref={searchInput} onChange={handleSearch} />
 
-        {posts?.map((post) => {
-          const PUBLISHED_TIME_AGO = getTimeAgo(
-            Number(new Date(post?.createdAt))
-          )
+        {posts?.map((post) => (
+          <PostItem key={post?.id} post={post} />
+        ))}
 
-          return (
-            <Link
-              key={post.id}
-              href={{
-                pathname: `/post/${post?.id}`,
-                query: {
-                  title: post?.title,
-                  description: post?.description,
-                  whatsApp: post?.whatsApp,
-                },
-              }}
-            >
-              <Box
-                marginBottom={8}
-                bgGradient="linear(to-bl, cyan.600, cyan.800)"
-                _hover={{
-                  bgGradient: 'linear(to-l, cyan.800, cyan.800)',
-                  color: 'cyan.400',
-                }}
-                borderRadius={8}
-                padding={8}
-                cursor="pointer"
-              >
-                <Box>
-                  <Flex alignItems="center">
-                    <Heading
-                      fontWeight={600}
-                      fontSize={{ base: 'xl', sm: 'xl', md: '2xl' }}
-                      lineHeight={'150%'}
-                      noOfLines={1}
-                    >
-                      {post.title}
-                    </Heading>
-                  </Flex>
-                  <Text color={'cyan.100'} marginTop={2}>
-                    {post.description}
-                  </Text>
-                </Box>
-
-                <Flex alignItems="center" marginTop={8}>
-                  <IconButton
-                    icon={<BsWhatsapp size={24} />}
-                    aria-label="Escribir mensaje vía WhatsApp"
-                    onClick={(e) => handleWhatsApp(e, post.title)}
-                    variant="ghost"
-                    size={'sm'}
-                    mr={4}
-                  />
-                  <IconButton
-                    icon={<FiShare2 size={24} />}
-                    aria-label="Compartir en redes sociales"
-                    onClick={(e) => handleShare(e, post)}
-                    variant="ghost"
-                    size={'sm'}
-                  />
-                  <Spacer />
-                  {PUBLISHED_TIME_AGO}
-                </Flex>
-              </Box>
-            </Link>
-          )
-        })}
         <Center display="flex" flexDirection={'column'} gap={10}>
           {SHOW_MORE_BUTTON && !IS_SEARCHING && (
             <Button
@@ -319,8 +156,6 @@ export async function getServerSideProps() {
   const res = await fetch(`${server}/posts/?page=${page}`)
   const errorCode = res.ok ? false : res?.status
   const response = await res.json()
-
-  console.log('response: ', response)
 
   return { props: { response, errorCode } }
 }
